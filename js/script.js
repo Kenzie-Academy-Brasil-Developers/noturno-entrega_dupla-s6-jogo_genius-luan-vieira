bbd4d1698da6adb034ed884b2cae10dc99805099
noomloof <bhtanikawa@gmail.com>
2022-04-01 16:31:53 -0400

    Desenvolvimento do Genius

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+let arrayJogador = []
+let order = []
+
+
 function createItems() {
 
     const box = document.querySelector('.box')
@@ -31,18 +35,56 @@ function createItems() {
     divGame.appendChild(divBlue)
     divGame.appendChild(divControl)
 
-    divGreen.addEventListener('click', coverColors)
-    divRed.addEventListener('click', coverColors)
-    divYellow.addEventListener('click', coverColors)
-    divBlue.addEventListener('click', coverColors)
-
-
 }
 
 createItems()
 
+function habilitarClick() {
+    let divGreen = document.getElementById('btn--green')
+    let divRed = document.getElementById('btn--red')
+    let divYellow = document.getElementById('btn--yellow')
+    let divBlue = document.getElementById('btn--blue')
 
+    divGreen.addEventListener('click', coverColors)
+    divRed.addEventListener('click', coverColors)
+    divYellow.addEventListener('click', coverColors)
+    divBlue.addEventListener('click', coverColors)
+}
 
+function desabilitarClick() {
+    let descendentes = document.querySelectorAll('.game__btn');
+    descendentes.forEach((element) => element.removeEventListener('click', clickJogador))
+
+    let divGreen = document.getElementById('btn--green')
+    let divRed = document.getElementById('btn--red')
+    let divYellow = document.getElementById('btn--yellow')
+    let divBlue = document.getElementById('btn--blue')
+
+    divGreen.removeEventListener('click', coverColors)
+    divRed.removeEventListener('click', coverColors)
+    divYellow.removeEventListener('click', coverColors)
+    divBlue.removeEventListener('click', coverColors)
+}
+
+function playGreen() {
+    let audio = document.getElementById('greenAudio')
+    audio.play()
+}
+
+function playRed() {
+    let audio = document.getElementById('redAudio')
+    audio.play()
+}
+
+function playYellow() {
+    let audio = document.getElementById('yellowAudio')
+    audio.play()
+}
+
+function playBlue() {
+    let audio = document.getElementById('blueAudio')
+    audio.play()
+}
 
 function coverColors() {
 
@@ -52,20 +94,20 @@ function coverColors() {
     let divBlue = document.getElementById('btn--blue')
 
     if (this.getAttribute('id') === 'btn--green') {
-        divGreen.style.backgroundColor = '#145011'
-
+        divGreen.classList.add('brilho','brilhoVerde')
+        playGreen()
     }
     if (this.getAttribute('id') === 'btn--red') {
-        divRed.style.backgroundColor = '#521517'
-
+        divRed.classList.add('brilho','brilhoVermelho')
+        playRed()
     }
     if (this.getAttribute('id') === 'btn--yellow') {
-        divYellow.style.backgroundColor = '#645731'
-
+        divYellow.classList.add('brilho','brilhoAmarelo')
+        playYellow()
     }
     if (this.getAttribute('id') === 'btn--blue') {
-        divBlue.style.backgroundColor = '#22575f'
-
+        divBlue.classList.add('brilho','brilhoAzul')
+        playBlue()
     }
     setTimeout(clearColor, 200)
 }
@@ -76,12 +118,35 @@ function clearColor() {
     let divYellow = document.getElementById('btn--yellow')
     let divBlue = document.getElementById('btn--blue')
 
+    divGreen.classList.remove('brilho','brilhoVerde')
+    divRed.classList.remove('brilho','brilhoVermelho')
+    divYellow.classList.remove('brilho','brilhoAmarelo')
+    divBlue.classList.remove('brilho','brilhoAzul')
 
     divGreen.style.backgroundColor = '#0ec108'
     divRed.style.backgroundColor = '#fb3840'
     divYellow.style.backgroundColor = '#ffc721'
     divBlue.style.backgroundColor = '#0dd1f1'
 }
+
+function delay() {
+    removeMenu()
+    createMemorize()
+    corAleatoria()
+    sequenciaDeCores()
+}
+
+function replay() {
+    order = []
+    createMemorize()
+    corAleatoria()
+    sequenciaDeCores()
+    habilitarClick()
+    pegaSequenciaJogador()
+}
+
+//----------------------------------------------------------------------------
+// tela do menu
 
 function createMenu() {
 
@@ -100,32 +165,11 @@ function createMenu() {
     menuButton.innerText = 'PLAY'
     menuButton.addEventListener('click', delay)
 
-
-
     divControl.appendChild(menuTextWelcome)
     divControl.appendChild(menuTextQuest)
     divControl.appendChild(menuButton)
 }
 createMenu()
-
-
-
-function delay() {
-
-
-    removeMenu()
-    createMemorize()
-    setTimeout(removeMemorize, 600)
-    setTimeout(piscaGreen, 1000)
-    setTimeout(createResponse, 3000)
-
-    pegaSequenciaJogador()
-
-
-}
-
-//----------------------------------------------------------------------------
-
 
 function removeMenu() {
 
@@ -157,8 +201,8 @@ function createMemorize() {
 
 function removeMemorize() {
 
-    const memorize = document.querySelector('.memorize')
-    memorize.style.display = `none`
+    const memorize = document.querySelectorAll('.memorize')
+    memorize.forEach(element => element.style.display = `none`)
 
 }
 
@@ -170,128 +214,191 @@ function createResponse() {
 
     const spanMemorize = document.createElement('span')
     spanMemorize.innerText = "It's your turn!\nClick the pads in order!"
-    spanMemorize.classList.add('memorize')
+    spanMemorize.classList.add('response')
 
     control.appendChild(spanMemorize)
 
 }
 
-//------------------------------------------------------------------------
+function removeResponse() {
+    const response = document.querySelectorAll('.response')
+    response.forEach(element => element.style.display = `none`)
+}
+
+//---------------------------------------------------------------------------------
+//mensagem que indica que o jogador errou
+function createGameOver() {
+    desabilitarClick()
+
+    const control = document.querySelector('.game__control')
+
+    const spanGameOver = document.createElement('span');
+    spanGameOver.innerText = `Game Over!\n\n Score: ${order.length - 1}`
+    spanGameOver.setAttribute('id', 'gameOver')
+    spanGameOver.classList.add('gameOverPart')
+
+    const buttonJogarNovamente = document.createElement('button')
+    buttonJogarNovamente.innerText = "PLAY AGAIN"
+    buttonJogarNovamente.setAttribute('id','playAgain')
+    buttonJogarNovamente.classList.add('gameOverPart')
+    buttonJogarNovamente.addEventListener('click', removeGameOver)
+    buttonJogarNovamente.addEventListener('click', replay)
+    
+    control.appendChild(spanGameOver)
+    control.appendChild(buttonJogarNovamente)
+}
+
+function removeGameOver() {
+    const gameOver = document.querySelectorAll('.gameOverPart')
+    gameOver.forEach((element) => element.style.display = `none`)
+}
+
+//---------------------------------------------------------------------------------
 
 //sequencia aleatoria
 
-let order = []
-let score = 0
+function corAleatoria() {
+    order.push(Math.floor(Math.random() * 4))
 
-
-/*
-//gera numero aleatorio de 0 a 3 (positions)
-function getRandomArbitrary(a, b) {
-    let teste = Math.floor(Math.random() * 4)
-    let retorno = order.push(teste)
-    return retorno
-}
-getRandomArbitrary()
-*/
-
-
-
-function randomColorPicker() {
-    let teste = order.push(Math.floor(Math.random() * 4))
-
-    console.log(teste)
 }
 
+function sequenciaDeCores() {
+    setTimeout(() => {
+        for(i = 0; i < order.length; i++) {
+            if(order[i] === 0) {
+                setTimeout(() => {
+                    piscaGreen()
+                }, 800 * i);
+            } else if (order[i] === 1) {
+                setTimeout(() => {
+                    piscaRed()
+                }, 800 * i);
+            } else if (order[i] === 2) {
+                setTimeout(() => {
+                    piscaYellow()
+                }, 800 * i);
+            } else if (order[i] === 3) {
+                setTimeout(() => {
+                    piscaBlue()
+                }, 800 * i);
+            }
+            if(i === order.length - 1) {
+                setTimeout(() => {
+                    vezDoJogador()
+                }, 800 * i + 800);
+            }
+        }
+    }, 500);
+}
 
-
+function vezDoJogador() {
+    removeMemorize()
+    createResponse()
+    setTimeout(() => {
+        pegaSequenciaJogador()
+        habilitarClick()    
+    }, 200);
+}
 
 function piscaGreen() {
     let divGreen = document.getElementById('btn--green')
-    divGreen.style.backgroundColor = '#145011'
-    setTimeout(desligaGreen, 200)
+    divGreen.classList.add('brilho','brilhoVerde')
+    playGreen()
+    setTimeout(desligaGreen, 400)
 }
 
 function desligaGreen() {
     let divGreen = document.getElementById('btn--green')
-    divGreen.style.backgroundColor = '#0ec108'
+    divGreen.classList.remove('brilho','brilhoVerde')
 }
 
 function piscaRed() {
     let divRed = document.getElementById('btn--red')
-    divRed.style.backgroundColor = '#521517'
-    setTimeout(desligaRed, 200)
+    divRed.classList.add('brilho','brilhoVermelho')
+    playRed()
+    setTimeout(desligaRed, 400)
 }
 
 function desligaRed() {
     let divRed = document.getElementById('btn--red')
-    divRed.style.backgroundColor = '#fb3840'
+    divRed.classList.remove('brilho','brilhoVermelho')
 }
 //---------------------------------------------------------------------------------------
 
-
 function piscaYellow() {
     let divYellow = document.getElementById('btn--yellow')
-    divYellow.style.backgroundColor = '#645731'
-    setTimeout(desligaYellow, 200)
+    divYellow.classList.add('brilho','brilhoAmarelo')
+    playYellow()
+    setTimeout(desligaYellow, 400)
 }
 
 function desligaYellow() {
     let divYellow = document.getElementById('btn--yellow')
-    divYellow.style.backgroundColor = '#ffc721'
+    divYellow.classList.remove('brilho','brilhoAmarelo')
 }
 
 function piscaBlue() {
     let divBlue = document.getElementById('btn--blue')
-    divBlue.style.backgroundColor = '#22575f'
-    setTimeout(desligaBlue, 200)
+    divBlue.classList.add('brilho','brilhoAzul')
+    playBlue()
+    setTimeout(desligaBlue, 400)
 }
 
 function desligaBlue() {
     let divBlue = document.getElementById('btn--blue')
-    divBlue.style.backgroundColor = '#0dd1f1'
+    divBlue.classList.remove('brilho','brilhoAzul')
 }
-
-
-
-function sequence() {
-
-    setTimeout(piscaGreen, 1000)
-    setTimeout(piscaBlue, 1500)
-    setTimeout(piscaGreen, 2000)
-    setTimeout(piscaRed, 2500)
-    setTimeout(piscaYellow, 3000)
-    setTimeout(piscaBlue, 3500)
-    setTimeout(piscaGreen, 4000)
-    setTimeout(piscaYellow, 4500)
-    setTimeout(piscaBlue, 5000)
-    setTimeout(piscaRed, 5500)
-    setTimeout(piscaBlue, 6000)
-    setTimeout(piscaRed, 6500)
-    setTimeout(piscaBlue, 7000)
-    setTimeout(piscaYellow, 7500)
-
-}
-
-let arrayJogador = []
-console.log(arrayJogador)
-
-
 
 function pegaSequenciaJogador() {
 
+    arrayJogador = [];
+
     let descendentes = document.querySelectorAll('.game__btn');
+    descendentes.forEach((element) => element.addEventListener('click', clickJogador))
+}
 
-    for (let i = 0; i < descendentes.length; i++) {
-        descendentes[i].addEventListener("click", function(e) {
+function clickJogador() {
+    arrayJogador.push(this.id)
 
-            arrayJogador.push(this.id)
-            console.log(arrayJogador)
+    tradutorDeArray()
+    checarSequencia()
+}
 
-
-        })
-
+function tradutorDeArray() {
+    for (let i = 0; i < arrayJogador.length; i++) {
+        if(arrayJogador[i] === 'btn--green') {
+            arrayJogador[i] = 0
+        }
+        if(arrayJogador[i] === 'btn--red') {
+            arrayJogador[i] = 1
+        }
+        if(arrayJogador[i] === 'btn--yellow') {
+            arrayJogador[i] = 2
+        }
+        if(arrayJogador[i] === 'btn--blue') {
+            arrayJogador[i] = 3
+        }
     }
+}
 
+function checarSequencia() { 
+    if(order[arrayJogador.length - 1] !== arrayJogador[arrayJogador.length - 1]) {
+        removeResponse()
+        createGameOver()
+    }  
+    if((order.length === arrayJogador.length) && (order[arrayJogador.length - 1] === arrayJogador[arrayJogador.length - 1])) {
+        sequenciaCorreta()
+    }
+}
+
+function sequenciaCorreta() {
+    setTimeout(() => {
+        desabilitarClick()
+        removeResponse()
+        createMemorize()
+        corAleatoria()
+        sequenciaDeCores()
+    }, 100);
 }
 
 //-------------------------------------------------------------------------------------------
